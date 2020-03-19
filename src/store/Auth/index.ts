2b38fb417a63bfd1513645e839315b8822262e6f
unknown <m.khmalech@gmail.com>
2020-03-19 15:16:11 +0100

    typinf file

--- src/store/Auth/index.ts
+++ src/store/Auth/index.ts
@@ -2,13 +2,6 @@ import { combineReducers } from "redux";
 import { LoginReducer } from './Login/reducer'
 import { SignupReducer } from './Signup/reducer'
 import { PassResetpReducer } from './PassReset/reducer'
-import { LoginPageState } from "./Login/types";
-
-export interface AuthStates {
-    Login       : LoginPageState
-    Signup      : String
-    PassReset   : String
-}
 
 
 export default combineReducers({
